7a118123b3ebf2802943a763b194a3bf587a289a
matton <masato_ikegawa612400@yahoo.co.jp>
2021-08-03 11:26:18 +0900

    uidはいらない

--- CMS/cms_poc/src/main/java/jp/kobeu/cs/daikibo/cms_poc/dto/UserResisterDto.java
+++ CMS/cms_poc/src/main/java/jp/kobeu/cs/daikibo/cms_poc/dto/UserResisterDto.java
@@ -3,16 +3,13 @@ import jdk.jfr.DataAmount;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-
 public class UserResistorDto {
-    Long uid;
     String username;
     String email;
 
     public UserResister toEntity() {
         User user = new User();
 
-        user.setUid(uid);
         user.setUsername(username);
         user.setEmail(email);
 
