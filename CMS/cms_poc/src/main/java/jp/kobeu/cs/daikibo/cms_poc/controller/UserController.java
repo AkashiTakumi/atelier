a69134e22ef2cf9d5efe2e02c479b1c4964c9c54
matton <masato_ikegawa612400@yahoo.co.jp>
2021-08-03 11:30:06 +0900

    register実装

--- CMS/cms_poc/src/main/java/jp/kobeu/cs/daikibo/cms_poc/controller/UserController.java
+++ CMS/cms_poc/src/main/java/jp/kobeu/cs/daikibo/cms_poc/controller/UserController.java
@@ -1,4 +1,4 @@
-
+import jp.kobeu.cs.daikibo.cms_poc.dto.UserResistorDto;
 
 @Controller
 public class UserController {
@@ -11,13 +11,18 @@ public class UserController {
         model.addAttribute("userList",list);
         model.addAttribute("userForm", new UserForm());
         // 仮テンプレ名
-        return "user_list"
+        return "user_list";
     }
 
     @GetMapping("/user/register")
     String userRegister(@ModelAttribute("userForm") UserForm form, Model model){
         User u = new User();
         // dto
+        UserResistorDto udto = new UserResistorDto();
+        udto.setUsername(form.username);
+        udto.setEmail(form.email);
+        us.userRegister(udto);
+        return "redirect:/user";
     }
 
 
