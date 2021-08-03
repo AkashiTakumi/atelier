b305a8489764cb98595d53bc7ea2b8990fd57cd8
kawanishi <kawanishi@fine.cs.kobe-u.ac.jp>
2021-08-03 10:20:36 +0900

    A

--- CMS/cms_poc/src/main/java/jp/kobeu/cs/daikibo/cms_poc/controller/ComicController.java
+++ CMS/cms_poc/src/main/java/jp/kobeu/cs/daikibo/cms_poc/controller/ComicController.java
@@ -14,33 +14,59 @@ public class ComicController {
     @Autowired
     ComicService cs;
 
-    // 漫画管理画面の表示
+    /**
+     * 漫画管理画面の表示
+     * 
+     * @param uid
+     * @param model
+     * @return
+     */
     @GetMapping("/read")
     String showComicList(Long uid, Model model) {
 
     }
 
-    // 新規登録漫画の確認画面表示
+    /**
+     * 新規登録漫画の確認画面表示
+     * 
+     * @param model
+     * @return
+     */
     @GetMapping("/read/check")
     String showComicCheckPage(Model model) {
 
     }
 
-    // 巻数のインクリメント
+    /**
+     * 巻数のインクリメント
+     * @param cid
+     * @param model
+     * @return
+     */
     @PostMapping("/read")
     String buy(Long cid, Model model){
         cs.buy(cid);
         return "redirect:/read";
     }
 
-    // 巻数のデクリメント
+    /**
+     * 巻数のデクリメント
+     * @param cid
+     * @param model
+     * @return
+     */
     @PostMapping("/read")
     String cancel(Long cid, Model model) {
         cs.cancel(cid);
         return "redirect:/read";
     }
 
-    // 漫画の新規登録
+    /**
+     * 漫画の新規登録
+     * @param title
+     * @param model
+     * @return
+     */
     @PostMapping("/read")
     String comicRegister(@RequestParam String title, Model model) {
         Comic comic = new Comic();
