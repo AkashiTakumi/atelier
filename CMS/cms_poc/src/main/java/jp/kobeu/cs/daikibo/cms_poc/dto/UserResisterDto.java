46da80000c3f7c2964fa60b77c03e68ed80a9cc4
matton <masato_ikegawa612400@yahoo.co.jp>
2021-08-03 10:10:01 +0900

    dto 雛形作成

---
+++ CMS/cms_poc/src/main/java/jp/kobeu/cs/daikibo/cms_poc/dto/UserResisterDto.java
@@ -0,0 +1,16 @@
+import jdk.jfr.DataAmount;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class UserResistorDto {
+    Long uid;
+    String username;
+    String email;
+
+    public UserResister name() {
+        
+    }
+}
+
