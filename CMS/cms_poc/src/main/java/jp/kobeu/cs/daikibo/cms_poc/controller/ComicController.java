9b1b09338654d69db54dba1a2231c9f3e2b0fa5c
kawanishi <kawanishi@fine.cs.kobe-u.ac.jp>
2021-08-03 10:13:57 +0900

    Fixed directory structure

--- CMS/cms_poc/src/main/java/jp/kobeu/cs/daikibo/cms_poc/controller/ComicController.java
+++ CMS/cms_poc/src/main/java/jp/kobeu/cs/daikibo/cms_poc/controller/ComicController.java
@@ -1,6 +1,4 @@
-package jp.kobe_u.cs.daikibo.CMS.controller;
-
-import java.util.List;
+package jp.kobe_u.cs.daikibo.cms_poc.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,31 +12,42 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class ComicController {
 
     @Autowired
-    ComicService ts;
+    ComicService cs;
 
+    // 漫画管理画面の表示
     @GetMapping("/read")
-    String showComicList(Model model) {
+    String showComicList(Long uid, Model model) {
 
     }
 
-    @GetMapping("/read")
+    // 新規登録漫画の確認画面表示
+    @GetMapping("/read/check")
     String showComicCheckPage(Model model) {
 
     }
 
+    // 巻数のインクリメント
     @PostMapping("/read")
     String buy(Long cid, Model model){
-
+        cs.buy(cid);
+        return "redirect:/read";
     }
 
+    // 巻数のデクリメント
     @PostMapping("/read")
     String cancel(Long cid, Model model) {
-
+        cs.cancel(cid);
+        return "redirect:/read";
     }
 
+    // 漫画の新規登録
     @PostMapping("/read")
     String comicRegister(@RequestParam String title, Model model) {
-
+        Comic comic = new Comic();
+        comic.setTitle(title);
+        // cid
+        cs.comicRegister(/* */);
+        return "redirect:/read/check";
     }
 
 }
